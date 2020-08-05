201757216bb421625e85e03c0f3c40c8e74b0a4b
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2020-08-05 04:12:30 +0100

    Data fetching implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+
 
 import "./App.css";
 
 function App() {
+  const API_BASE_URL = "http://nyx.vima.ekt.gr:3000/api/books";
+  const ITEMS_PER_PAGE = 2;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [resultsCount, setResultsCount] = useState<number>(0);
+  const [books, setBooks] = useState<BookDetails[]>([]);
+
+  useEffect(() => {
+    fetch(API_BASE_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        page: currentPage,
+        itemsPerPage: ITEMS_PER_PAGE,
+        filters: [],
+      }),
+    })
+      .then((results) => results.json())
+      .then((data) => {
+        setResultsCount(data.count);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [currentPage]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Book Registry</h2>
+        <Container>
+          <h2>Book Registry</h2>
+          <div>All results: {resultsCount}</div>
+        </Container>
       </header>
     </div>
   );
